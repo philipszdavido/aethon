15a63faa0052fbd717e1fb2d661379ad11b6bd7b
philipsz-davido <philipsz-davido>
2018-05-09 08:51:56 +0100

    Updated Readme

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ __Aethon.js__ is a fast JavaScript library for building web apps.
 # Code Example
 
 ```js
-import Aethon, {Component} from 'aethon'
+import Aethon, { Component } from 'aethon'
 import AethonDOM from 'aethon-dom'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <a href=''>Click here</a>
+                <a href=''> Click here</a>
             </div>
         )
     }
@@ -25,4 +25,16 @@ AethonDOM.render(<App />,document.getElementById('root'))
 
 ```sh
 npm install aethon aethon-dom -S
+```
+
+OR you can use our CLI tool, `aethon-cli` for easy scaffold of Aethon projects:
+
+```sh
+npm i aethon-cli -g
+```
+
+Scaffold new project:
+
+```sh
+aeth new aethon-project
 ```
--- packages/aethon-dom/dist/aethon-dom.js
+++ packages/aethon-dom/dist/aethon-dom.js
@@ -1,68 +1,9 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('aethon-shared')) :
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('aethon-shared')) :
     typeof define === 'function' && define.amd ? define(['aethon-shared'], factory) :
-    (global.AethonDOM = factory(null));
+    (factory(null));
 }(this, (function (aethonShared) { 'use strict';
 
-    function mount(element, parentDom) {
-        if (element.type == aethonShared.VNodeTypes.COMPONENT) {
-            return mountComponent(element.tag, parentDom)
-        }
-        if (element.type == aethonShared.VNodeTypes.ELEMENT) {
-            return mountElement(element, parentDom)
-        }
-        if (!element.type) {
-            return mountText(element, parentDom)
-        }
-    }
 
-    function mountComponent(Component, parentDom) {
-        const component = new Component();
-        component.componentWillMount();
-        const dom = mount(component.render(), parentDom);
-        component.componentDidMount();
-        return dom
-    }
-
-    function mountElement(element, parentDom) {
-        const { type, props, tag, children } = element;
-        const dom = document.createElement(tag);
-        parentDom.appendChild(dom);
-        appendProps(props, dom);
-        mountArrayChildren(children, dom);
-        return dom
-    }
-
-    function mountArrayChildren(children, dom) {
-        for (var index = 0; index < children.length; index++) {
-            var child = children[index];
-            mount(child, dom);
-        }
-    }
-
-    function mountText(element, parentDom) {
-        const dom = document.createTextNode(element);
-        parentDom.appendChild(dom);
-        return element
-    }
-
-    function appendProps(props, element) {
-        for (var key in props) {
-            element.setAttribute(key, props[key]);
-        }
-    }
-
-    const AethonDOM = {
-        render: (_yag, root) => {
-            const element = _yag;
-            const parentDom = root;
-
-            const dom = mount(element, parentDom);
-            parentDom.appendChild(dom);
-            return dom
-        }
-    };
-
-    return AethonDOM;
 
 })));
